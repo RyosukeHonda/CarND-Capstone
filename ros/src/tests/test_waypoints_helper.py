b4525a2180530c6ef09a2ce1f392832035b4694b
Jakub Kolodziejczyk <jakub.kolodziejcyzk@mail.com>
2017-08-21 20:23:33 -0700

    Added get_distance_between_points() and get_closest_waypoint_index() to waypoints_helper.py

--- ros/src/tests/test_waypoints_helper.py
+++ ros/src/tests/test_waypoints_helper.py
@@ -2,9 +2,47 @@
 Tests for waypoints helper
 """
 
+import styx_msgs.msg
+import geometry_msgs.msg
+import numpy as np
+
 import src.waypoint_updater.waypoints_helper as waypoints_helper
 
 
-def test_simple():
+def test_get_distance_between_points():
 
-    assert 0 == waypoints_helper.get_closest_waypoint_index(1, 2)
+    first = geometry_msgs.msg.Point()
+    first.x = 0
+    first.y = 0
+
+    second = geometry_msgs.msg.Point()
+    second.x = 10
+    second.y = 20
+
+    expected = np.sqrt(500)
+    actual = waypoints_helper.get_distance_between_points(first, second)
+
+    assert np.isclose(expected, actual)
+
+
+def test_get_closest_waypoint_index():
+
+    pose = geometry_msgs.msg.Pose()
+    pose.position.x = 10
+    pose.position.y = 10
+
+    first_waypoint = styx_msgs.msg.Waypoint()
+    first_waypoint.pose.pose.position.x = 0
+    first_waypoint.pose.pose.position.y = 0
+
+    second_waypoint = styx_msgs.msg.Waypoint()
+    second_waypoint.pose.pose.position.x = 8
+    second_waypoint.pose.pose.position.y = 8
+
+    third_waypoint = styx_msgs.msg.Waypoint()
+    third_waypoint.pose.pose.position.x = 20
+    third_waypoint.pose.pose.position.y = 20
+
+    waypoints = [first_waypoint, second_waypoint, third_waypoint]
+
+    assert 1 == waypoints_helper.get_closest_waypoint_index(pose, waypoints)
--- ros/src/waypoint_updater/waypoints_helper.py
+++ ros/src/waypoint_updater/waypoints_helper.py
@@ -2,10 +2,29 @@
 Utilities used by waypoints_updater
 """
 
+import numpy as np
+
+
+def get_distance_between_points(first, second):
+
+    x_difference = first.x - second.x
+    y_difference = first.y - second.y
+
+    return np.sqrt(x_difference**2 + y_difference**2)
+
 
 def get_closest_waypoint_index(pose, waypoints):
 
     best_index = 0
+    best_distance = get_distance_between_points(pose.position, waypoints[0].pose.pose.position)
 
-    # for index, waypoint in enumerate(waypoints):
+    for index, waypoint in enumerate(waypoints):
+
+        distance = get_distance_between_points(pose.position, waypoint.pose.pose.position)
+
+        if distance < best_distance:
+
+            best_index = index
+            best_distance = distance
+
     return best_index
