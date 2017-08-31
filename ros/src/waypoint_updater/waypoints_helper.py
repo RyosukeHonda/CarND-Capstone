5421c27f7940200a2fbe779dfd81a160dbf546a4
Jakub Kolodziejczyk <jakub.kolodziejcyzk@mail.com>
2017-08-30 17:22:46 -0700

    Removed get_sublist() implementation that broke dbw_node

--- ros/src/waypoint_updater/waypoints_helper.py
+++ ros/src/waypoint_updater/waypoints_helper.py
@@ -53,25 +53,18 @@ def get_closest_waypoint_index(position, waypoints_matrix):
 def get_sublist(elements, start_index, size):
     """
     Given a list of elements, start index and size of sublist, returns
-    sublist starting from start_index that has size elements. The sublist is made
-    of waypoints before and after the car.
+    sublist starting from start_index that has size elements. Takes care of wrapping around should
+    start_index + size > len(elements)
     :param elements: list
     :param start_index: start index
     :param size: size of sublist
     :return: sublist, wrapped around beginning of elements list if necessary
     """
-    
-    size = size / 2
-    start_index -= size
-    if start_index < 0:
-       start_index = len(elements) + start_index
 
-    sublist = []
-    for i in range(size):
-        start_index = start_index % (len(elements) - 1)
-        sublist.append(elements[start_index])
-        start_index += 1
-    return sublist
+    # A very simple, not necessarily efficient solution
+    doubled_elements = elements + elements[:size]
+    return doubled_elements[start_index: start_index + size]
+
 
 def get_smoothed_out_waypoints(waypoints):
     """
