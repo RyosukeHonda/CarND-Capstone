b6bd20f7500cf438bda59b1e7a5f74a064f700b8
Jakub Kolodziejczyk <jakub.kolodziejcyzk@mail.com>
2017-08-31 06:52:29 -0700

    Added a simple pid control for braking. Far from perfect, but car slows down to a crawl near red lights

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -7,13 +7,14 @@ ONE_MPH = 0.44704
 
 class Controller(object):
 
-    def __init__(self, throttle_pid, steering_pid):
+    def __init__(self, throttle_pid, brake_pid, steering_pid):
 
         self.throttle_pid = throttle_pid
+        self.brake_pid = brake_pid
         self.steering_pid = steering_pid
 
         self.throttle_filter = lowpass.SmoothingFilter(window_weight=0.8)
-        self.brake_filter = lowpass.SmoothingFilter(window_weight=0.2)
+        self.brake_filter = lowpass.SmoothingFilter(window_weight=0.0)
         self.steering_filter = lowpass.SmoothingFilter(window_weight=0.5)
 
     def control(self, linear_velocity_error, cross_track_error, sample_time):
@@ -23,11 +24,17 @@ class Controller(object):
 
         brake = 0
 
-        if throttle < 0:
+        if throttle > 0:
 
-            brake = 200
-            brake = self.brake_filter.get_smoothed_value(brake)
+            # Update brake value in filter with 0
+            self.brake_filter.get_smoothed_value(0)
+
+        else:
+
             throttle = 0
+
+            brake = self.brake_pid.step(-linear_velocity_error, sample_time)
+            brake = self.brake_filter.get_smoothed_value(brake)
 
         steering = self.steering_pid.step(cross_track_error, sample_time)
 
