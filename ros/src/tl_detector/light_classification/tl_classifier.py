4e5340962616004624fc84df950ba0d641ede075
RyosukeHonda <hondaryo00888005@gmail.com>
2017-09-04 00:50:16 -0700

    revised TLClassifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -6,18 +6,12 @@ import tensorflow as tf
 import json
 import rospy
 
-class TLClassifier(object):
+class TLClassifierCV(object):
 
     def __init__(self):
         # Lower and Upper threshold for color extraction
         self.lower = np.array([150, 100, 150])
         self.upper = np.array([180, 255, 255])
-
-        with open('light_classification/saved_models/model128.json', 'r') as f:
-            loaded_model_json = f.read()
-        self.model = model_from_json(loaded_model_json)
-        self.model.load_weights('light_classification/saved_models/weights128.hdf5')
-        self.graph = tf.get_default_graph()
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -29,35 +23,19 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        # state = TrafficLight.UNKNOWN
+        state = TrafficLight.UNKNOWN
 
-        # red_area, red_image = self.get_colored_area(image, self.lower, self.upper)
+        red_area, red_image = self.get_colored_area(image, self.lower, self.upper)
 
         # Needs careful tuning for the number of red pixesls
-        # red_pixels = 40
+        red_pixels = 40
 
-        # if red_area > red_pixels:
-        #     state = TrafficLight.RED
+        if red_area > red_pixels:
+            state = TrafficLight.RED
 
-        # return state
-
-
-        image = self.process_image(image)
-
-        with self.graph.as_default():
-            pred_class = self.model.predict_classes(image, batch_size = 1, verbose = 0)
-            # rospy.logwarn("Predicted class:{}".format(pred_class))
-            if pred_class == 0:
-                state = TrafficLight.RED
-            elif pred_class == 1:
-                state = TrafficLight.YELLOW
-            elif pred_class == 2:
-                state = TrafficLight.GREEN
-            else:
-                state = TrafficLight.UNKNOWN
         return state
 
-        
+ 
     def get_colored_area(self, image, lower, upper):
 
         hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -69,11 +47,39 @@ class TLClassifier(object):
         return area, extracted_image
 
 
-    def process_image(self, image):
+class TLClassifier(object):
 
-        desired_shape = (128, 128)
-        image = cv2.resize(image, desired_shape, cv2.INTER_LINEAR)
-        image = image.astype('float32') / 255
-        processed_image = image.reshape(1, *image.shape)
-        return processed_image 
+	def __init__(self):
 
+		with open('light_classification/saved_models/model128.json', 'r') as f:
+			loaded_model_json = f.read()
+		self.model = model_from_json(loaded_model_json)
+		self.model.load_weights('light_classification/saved_models/weights128.hdf5')
+		self.graph = tf.get_default_graph()
+
+	def get_classification(self, image):
+		"""Determines the color of the traffic light in the image
+
+        Args:
+            image (cv::Mat): image containing the traffic light
+
+        Returns:
+            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+
+        """
+
+        	processed_image = self.process_image(image)
+        	with self.graph.as_default():
+        		predicted_class_id = self.model.predict_classes(processed_image, batch_size = 1, verbose = 0)
+        		states_map = {0: TrafficLight.RED, 1: TrafficLight.YELLOW, 2: TrafficLight.GREEN}
+        		# Get code for predicted state, return unknown if couldn't classify
+        		state = states_map.get(predicted_class_id[0], TrafficLight.UNKNOWN)
+        	return state
+
+	def process_image(self, image):
+		desired_shape = (128, 128)
+		image = cv2.resize(image, desired_shape, cv2.INTER_LINEAR)
+		image = image.astype('float32') / 255
+		processed_image = image.reshape(1, *image.shape)
+
+		return processed_image 
